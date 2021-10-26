771650ab9bc582a07a5a34093b16c79d080e8b4d
Seeun <duck_sim@naver.com>
2021-10-26 09:49:32 +0900

    201814081-5

--- Hello.java
+++ Hello.java
@@ -3,9 +3,9 @@ package git_exam2;
 public class Hello {
 
 	public static void main(String[] args) {
-		System.out.println("IT융합자율학부");
-		System.out.println("201814081");
-		System.out.println("조세은");
+		System.out.println("소속: IT융합자율학부");
+		System.out.println("학번: 201814081");
+		System.out.println("이름: 조세은");
 	}
 
 }
